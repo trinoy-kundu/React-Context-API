37f9a881100394807b821e5f2bff8b6ff766bcb0
Trinoy Kundu <trinoykundu@gmail.com>
2026-02-18 14:58:16 +0600

    refactor: render components using React Contect API

--- src/Contexts.jsx/themeContext.jsx
+++ src/Contexts.jsx/themeContext.jsx
@@ -1,6 +1,12 @@
-import createContext from "../Library/Context";
+// import createContext from "../Library/Context";
 
-const themeContext = createContext({
+// const themeContext = createContext({
+//     theme: 'light'
+// });
+
+import React from "react";
+
+const themeContext = React.createContext({
     theme: 'light'
 });
 
