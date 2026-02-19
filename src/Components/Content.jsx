885b04950e7d370df9c61856e87b5927df64384d
Trinoy Kundu <trinoykundu@gmail.com>
2026-02-19 20:45:05 +0600

    refactor: convert Content to function and use Hook(useContext) instead of Consumer

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,30 +1,26 @@
 import Counter from './Counter';
 import HoverCounter from './HoverCounter';
 import ThemeContext from '../Contexts.jsx/themeContext';
-import React from 'react';
+import { useContext } from 'react';
 
-export default class Content extends React.Component {
+export default function Content() {
+    const context = useContext(ThemeContext);
+    const {theme, switchTheme} = context;
 
-    // static contextType = ThemeContext (another way to use contextType)
-    render() {
-        const {theme, switchTheme} = this.context;
-        return (
+    return (
         <div>
             <h1>This is a content</h1>
             <Counter>
                 {(count, incrementCount) => (
                     <HoverCounter
-                                count={count}
-                                incrementCount={incrementCount}
-                                theme={theme}
-                                switchTheme={switchTheme}
-                            />
+                        count={count}
+                        incrementCount={incrementCount}
+                         theme={theme}
+                        switchTheme={switchTheme}
+                    />
                 )}
             </Counter>
             
         </div>
     );
-    }
 }
-
-Content.contextType = ThemeContext;
